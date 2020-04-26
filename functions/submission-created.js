3eebe1c85b178ea36861f418d6fd6a4f732d126c
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-26 14:52:51 -0400

    feat: fix json package

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,36 +1,44 @@
 /* eslint-disable no-undef */
+
+//https://graphql.fauna.com/graphql
+// process.env.FAUNA_API_SECRET
+
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
+  console.log(event);
   const body = JSON.parse(event.body);
-  const { firstname, lastname, email } = body.payload.data;
+  const { firstname, lastname, email} = body.payload.data;
 
   const response = await fetch('https://graphql.fauna.com/graphql', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
-    },
-    body: JSON.stringify({
-      query: `
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
+      },
+
+      body: JSON.stringify( {
+        query: `
         mutation($name: String!, $email: String!) {
           createRegistration(data: { name: $name, email: $email }) {
             _id
           }
-        }      
-      `,
-      variables: {
-        name: `${firstname} ${lastname}`,
-        email,
-      },
-    }),
-  })
-    .then((res) => res.json())
-    .catch((err) => console.error(err));
+        }
+        
+         `,
+         variables: {
+            name: `${firstname} ${lastname}`, 
+            email,
+          },
+        }),
+      })
 
-  console.log(response);
+      .then((res) => res.json())
+      .catch((err) => console.log error(err));
 
-  return {
-    statusCode: 200,
-    body: 'boop',
-  };
-};
+      console.log(response);
+
+      return {
+        statusCode: 200,
+        body: 'boop',
+      };
+    };
