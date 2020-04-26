1c8d34b1d7944821077dfa3a627a8647d769348e
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-26 15:11:38 -0400

    feat: fix json package

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,36 +3,32 @@
 //https://graphql.fauna.com/graphql
 // process.env.FAUNA_API_SECRET
 
-more
-
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
   const body = JSON.parse(event.body);
-  const { firstname, lastname, email } = body.payload.data;
+  const { firstname, lastname, email} = body.payload.data;
 
   const response = await fetch('https://graphql.fauna.com/graphql', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
-    },
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
+      },
 
-      body: JSON.stringify( {
+      body: JSON.stringify({
         query: `
-        mutation($name: String!, $email: String!) {
-          createRegistration(data: { name: $name, email: $email }) {
-            _id
-          }
-        }
-        
-         `,
-         variables: {
-            name: `${firstname} ${lastname}`, 
-            email,
-          },
-        }),
-      })
-
+          mutation($name: String!, $email: String!) {
+            createRegistration(data: { name: $name, email: $email }) {
+              _id
+            }
+          }      
+        `,
+        variables: {
+          name: `${firstname} ${lastname}`,
+          email,
+        },
+      }),
+    })
       .then((res) => res.json())
       .catch((err) => console.error(err));
   
