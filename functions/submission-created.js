d0611c66b2800f963ec24b0b8ba1197fec0d333d
Sergio Rosa <scarlosjazz@gmail.com>
2020-04-26 13:29:23 -0400

    feat: write registration to Fauna

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,9 +1,42 @@
+/* eslint-disable no-undef */
+
+//https://graphql.fauna.com/graphql
+// process.env.FAUNA_API_SECRET
 
 exports.handler = async (event) => {
   console.log(event);
+  const body = JSON.parse(event.body);
+  const { firstname, lastname, email} = body.payload.data;
 
-  return {
-    statusCode: 200,
-    body: 'boop',
-  };
-};
+  const response = await fetch(
+    'https://graphql.fauna.com/graphql',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
+      },
+      body: {
+        query: `
+            mutation ($name: string! $email:string!) {
+            createRegistration(data:{
+              name: $name
+              email: $ email
+            }) {
+              _id
+            }
+          }
+         `,
+         variables: { name: `${firstname} ${lastname}`, email},
+        },
+      })
+
+      .then(res => res.json())
+      .catch(err => console.log error(err));
+
+      console.log(response);
+
+      return {
+        statusCode: 200,
+        body: 'boop',
+      };
+    };
